dd56e783f2b35c2b2adca299881dbe2caafe147e
Camilo Rodriguez <camiepisode@outlook.com>
2022-09-14 09:04:24 -0500

    Fix: Corrected identation tabs

--- src/core/device/settings/settings.cpp
+++ src/core/device/settings/settings.cpp
@@ -17,27 +17,27 @@
 
 Settings::Settings(String filename)
 {
-  _filename = filename;
-  _localStorage = new LocalStorage();
-  _settingsJson = _localStorage->read(filename);
+    _filename = filename;
+    _localStorage = new LocalStorage();
+    _settingsJson = _localStorage->read(filename);
 
-  if (_settingsJson.length() > 0)
-  {
-    setProperties();
-  }
+    if (_settingsJson.length() > 0)
+    {
+        setProperties();
+    }
 }
 
 void Settings::setProperties()
 {
-  DynamicJsonDocument doc(1024);
-  deserializeJson(doc, _settingsJson.c_str());
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, _settingsJson.c_str());
 
-  // FIXME: Set the settings
-  /* settings.device_id = doc["id"];
-  settings.is_wifi_enabled = doc["is_wifi_enabled"];
-  settings.is_api_enabled = doc["is_api_enabled"];
-  settings.is_ble_enabled = doc["is_ble_enabled"];
-  settings.is_flash_mode = doc["is_flash_mode"];
-  settings.settings_filename = doc["settings_filename"];
-  settings.flash_mode_delay = doc["flash_mode_delay"]; */
+    // FIXME: Set the settings
+    /* settings.device_id = doc["id"];
+    settings.is_wifi_enabled = doc["is_wifi_enabled"];
+    settings.is_api_enabled = doc["is_api_enabled"];
+    settings.is_ble_enabled = doc["is_ble_enabled"];
+    settings.is_flash_mode = doc["is_flash_mode"];
+    settings.settings_filename = doc["settings_filename"];
+    settings.flash_mode_delay = doc["flash_mode_delay"]; */
 }
--- src/core/device/settings/settings.h
+++ src/core/device/settings/settings.h
@@ -22,64 +22,64 @@
 class Settings
 {
 public:
-  Settings(String filename);
-  void setProperties();
+    Settings(String filename);
+    void setProperties();
 
-  typedef struct device_config
-  {
-    /* Communications */
-    // WIFI
-    static bool is_wifi_enabled = true;
-    static bool is_wifi_static_ip_enabled = false;
-    static int wifi_server_ip[4] = {192, 168, 1, 2};
-    static String wifi_prod_ssid = "Bookish default prod ssid";
-    static String wifi_prod_pass = "B00k1sh";
-    static String wifi_dev_ssid = "Bookish default dev ssid";
-    static String wifi_dev_pass = "B00k1sh";
+    typedef struct device_config
+    {
+        /* Communications */
+        // WIFI
+        static bool is_wifi_enabled = true;
+        static bool is_wifi_static_ip_enabled = false;
+        static int wifi_server_ip[4] = {192, 168, 1, 2};
+        static String wifi_prod_ssid = "Bookish default prod ssid";
+        static String wifi_prod_pass = "B00k1sh";
+        static String wifi_dev_ssid = "Bookish default dev ssid";
+        static String wifi_dev_pass = "B00k1sh";
 
-    // API Server
-    static bool is_api_enabled = true;
-    static bool is_wifiserver_enabled = false;
-    static String api_dev_origin = "192.168.1.2";
-    static String api_prod_origin = "api.bookish.com";
+        // API Server
+        static bool is_api_enabled = true;
+        static bool is_wifiserver_enabled = false;
+        static String api_dev_origin = "192.168.1.2";
+        static String api_prod_origin = "api.bookish.com";
 
-    // WIFI Access Point
-    static bool is_wifiap_enabled = false;
-    static String ap_ssid = "Bookish default ssid";
-    static String ap_pass = "B00k1sh";
-    static int wifi_ap_gateway[4] = {192, 168, 1, 254};
-    static int wifi_ap_subnet[4] = {255, 255, 255, 0};
-    static int wifi_ap_primary_dns[4] = {8, 8, 8, 8};
-    static int wifi_ap_secondary_dns[4] = {8, 8, 4, 4};
+        // WIFI Access Point
+        static bool is_wifiap_enabled = false;
+        static String ap_ssid = "Bookish default ssid";
+        static String ap_pass = "B00k1sh";
+        static int wifi_ap_gateway[4] = {192, 168, 1, 254};
+        static int wifi_ap_subnet[4] = {255, 255, 255, 0};
+        static int wifi_ap_primary_dns[4] = {8, 8, 8, 8};
+        static int wifi_ap_secondary_dns[4] = {8, 8, 4, 4};
 
-    // Bluetooth Low Energy (BLE)
-    static bool is_ble_enabled = true;
-    static String service_uuid "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
-    static String characteristic_uuid "beb5483e-36e1-4688-b7f5-ea07361b26a8";
+        // Bluetooth Low Energy (BLE)
+        static bool is_ble_enabled = true;
+        static String service_uuid "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
+        static String characteristic_uuid "beb5483e-36e1-4688-b7f5-ea07361b26a8";
 
-    // Over The Air (OTA)
-    static bool is_ota_enabled = true;
-    static String ota_password = "B00k1sh_4dm1n_OTA";
+        // Over The Air (OTA)
+        static bool is_ota_enabled = true;
+        static String ota_password = "B00k1sh_4dm1n_OTA";
 
-    // Developer
-    static bool is_developer_mode = true;
+        // Developer
+        static bool is_developer_mode = true;
 
-    /* Device */
-    static long main_thread_cycle = 100;
-    static String device_id = "nod-1c29d6b882";
-    static String device_name = "d-0012";
-    static String settings_filename = "settings.json";
+        /* Device */
+        static long main_thread_cycle = 100;
+        static String device_id = "nod-1c29d6b882";
+        static String device_name = "d-0012";
+        static String settings_filename = "settings.json";
 
-    static std::vector<route> routes;
-    static std::vector<Sensor> sensors;
-  };
+        static std::vector<route> routes;
+        static std::vector<Sensor> sensors;
+    };
 
-  struct device_config settings;
+    struct device_config settings;
 
 private:
-  String _filename;
-  String _settingsJson;
-  LocalStorage *_localStorage;
+    String _filename;
+    String _settingsJson;
+    LocalStorage *_localStorage;
 };
 
 #endif
