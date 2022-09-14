14fd88f8f2301433d820fcd5a1335d2c8dc00f0f
Camilo Rodriguez <camiepisode@outlook.com>
2022-09-14 09:04:47 -0500

    Fix: Corrected identation tabs

--- src/sensors/sensor/sensor.h
+++ src/sensors/sensor/sensor.h
@@ -17,36 +17,36 @@
 
 typedef struct Sensor
 {
-  String name;
-  String node_id;
-  String technical_name;
-  String title;
-  String description;
-  int interval;
-  int analog_0;
-  int analog_1;
-  int analog_2;
-  int analog_3;
-  int analog_4;
-  int analog_5;
-  int digital_0;
-  int digital_1;
-  int digital_2;
-  int digital_3;
-  int digital_4;
-  int digital_5;
-  int tx_0;
-  int rx_0;
-  int dio_0;
-  int dio_1;
-  int dio_2;
-  int dio_3;
-  int dio_4;
-  int dio_5;
-  int nss_0;
-  int sck_0;
-  int mosi_0;
-  int miso_0;
+    String name;
+    String node_id;
+    String technical_name;
+    String title;
+    String description;
+    int interval;
+    int analog_0;
+    int analog_1;
+    int analog_2;
+    int analog_3;
+    int analog_4;
+    int analog_5;
+    int digital_0;
+    int digital_1;
+    int digital_2;
+    int digital_3;
+    int digital_4;
+    int digital_5;
+    int tx_0;
+    int rx_0;
+    int dio_0;
+    int dio_1;
+    int dio_2;
+    int dio_3;
+    int dio_4;
+    int dio_5;
+    int nss_0;
+    int sck_0;
+    int mosi_0;
+    int miso_0;
 };
 
 #endif
