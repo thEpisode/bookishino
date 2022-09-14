dd490faecc51600ce983f9e22b3b772e24fe6501
Camilo Rodriguez <camiepisode@outlook.com>
2022-09-14 09:03:46 -0500

    Fix: Corrected identation tabs

--- src/core/connectivity/api/api.h
+++ src/core/connectivity/api/api.h
@@ -22,31 +22,31 @@
 
 namespace Core
 {
-  class Api
-  {
-  public:
-    Api(String origin, std::vector<route> routes);
-    DynamicJsonDocument deserialize(String json);
-    std::string serialize(JsonObject json);
-    String requestPayload(String endpoint, String payload, String method);
-    String requestParameters(String endpoint, String parameters, String method);
-    String getRoute(String name);
-    String getMethod(String name);
-    void handleAPServerRoutes(WiFiClient client);
-    String getOrigin();
+    class Api
+    {
+    public:
+        Api(String origin, std::vector<route> routes);
+        DynamicJsonDocument deserialize(String json);
+        std::string serialize(JsonObject json);
+        String requestPayload(String endpoint, String payload, String method);
+        String requestParameters(String endpoint, String parameters, String method);
+        String getRoute(String name);
+        String getMethod(String name);
+        void handleAPServerRoutes(WiFiClient client);
+        String getOrigin();
 
-  private:
-    String _origin;
-    String _endpoint;
-    std::vector<route> _routes;
-  };
+    private:
+        String _origin;
+        String _endpoint;
+        std::vector<route> _routes;
+    };
 } // namespace core
 
 struct route
 {
-  String name;
-  String endpoint;
-  String method;
+    String name;
+    String endpoint;
+    String method;
 };
 
 #endif
